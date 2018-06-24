690ea50d74b2f670510fff251064cb35b6768f16
Perry Brandiezs <pbrandiezs@gmail.com>
2018-06-24 07:28:22 -0700

    Added ex1 code to repo

---
+++ ex1.py
@@ -0,0 +1,11 @@
+name = input("Give me your name: ")
+print("Your name is " + name)
+age = input("What is your age? ")
+age = int(age)
+
+print("Your age is " + str(age))
+
+yearsToHundred = 100 - age
+yearWhenHundred = 2018 + yearsToHundred
+
+print("You will turn 100 in " + str(yearWhenHundred))
